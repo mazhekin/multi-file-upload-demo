a9d9960d74af97cc50ead5a140280a5bf6fb18c2
mazhekin <mazhekin@gmail.com>
2013-08-17 18:06:05 +0300

    first commit

---
+++ MvcApplication/Controllers/FilesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication4.Controllers
+{
+    public class FilesController : Controller
+    {
+        //
+        // GET: /Files/
+
+        public ActionResult Upload(HttpPostedFileBase[] file1)
+        {
+            foreach (var file in file1)
+            {
+                SaveFile(file.FileName, file.ContentType, file.InputStream);
+            }
+            return RedirectToAction("index", "home");
+        }
+
+        private void SaveFile(string fileName, string contentType, Stream inputStream)
+        {
+            using (var fileStream = System.IO.File.Create("D:\\Tests\\" + fileName))
+            {
+                inputStream.CopyTo(fileStream);
+            }
+        }
+    }
+}
---
+++ MvcApplication/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index(HttpPostedFileBase[] file1)
+        {
+            return View();
+        }
+
+        private void SaveFile(string fileName,string contentType, Stream inputStream)
+        {
+ 	        throw new NotImplementedException();
+        }
+
+    }
+}
